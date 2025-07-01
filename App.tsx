e32cc0d84b1bd60aad278283814f946ae8ec8990
Youssef2i <youseftourad@gmail.com>
2025-07-01 16:13:36 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-center p-10 text-white bg-gray-900 min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to Figrash</h1>
+      <p className="mt-4">Your Destiny 2 Service Marketplace</p>
+    </div>
+  )
+}
+
+export default App
